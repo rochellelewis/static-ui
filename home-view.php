82b93fee7a2b2fc5a873840cb563688ec7374ee2
Rochelle Lewis <rlewis37@gmail.com>
2017-11-27 15:59:08 -0700

    setup base files

---
+++ home-view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+	<!-- inject head-utils -->
+	<?php require_once ("head-utils.php");?>
+
+	<body class="sfooter">
+		<div class="sfooter-content">
+
+		</div>
+
+		<footer>
+
+		</footer>
+	</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rochelle
+ * Date: 11/27/17
+ * Time: 3:53 PM
+ */
